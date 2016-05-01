ebcee0b0ff9899b097a9e0fb063f29326b1b3325
adica <carmeladi@gmail.com>
2016-05-01 12:24:20 +0300

    build service

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,13 +1,30 @@
-class HomeController {
-  constructor(socialCountService) {
-  	'ngInject';
-    this.name = 'home';
-    this.socialCountService = socialCountService;
-  }
+import socialProvider from './../../shared/socialProvider';
 
-  get socialCountResult(){
-  	return this.socialCountService.getFacebookData;
-  }
+class HomeController {
+    constructor(socialCountService) {
+        'ngInject';
+        this.name = 'home';
+        this.url = "http://www.google.com";
+        this.socialNetworksList = [{type : "facebook"},{type: "pinterest"}];
+        this.selectedNetwork = "facebook"; //defualt value
+        this.socialCountService = socialCountService;
+        this.socialCountResult = {};
+    }
+
+    onGetData() {
+    	const provider = new socialProvider(this.selectedNetwork);
+        this.socialCountService.getSocialCount(provider, this.url).then((data) => {
+            this.socialCountResult = data;
+        });
+    }
+
+    get socialCount() {
+        return this.socialCountResult;
+    }
+
+    set socialCount(val) {
+        this.socialCountResult = val;
+    }
 }
 
 export default HomeController;
--- client/app/shared/socialCountService.js
+++ client/app/shared/socialCountService.js
@@ -1,4 +1,3 @@
-import socialProvider from './socialProvider';
 
 class socialCountService {
 	constructor($http, $q){
@@ -7,27 +6,23 @@ class socialCountService {
 		this.$q = $q;
 		this.socialCountObj = {};
 		this.facebookAPI = "https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%27http://www.google.com%27";
-	}
-	
-	
+	}	
 
-	_getSocialCount(provider, pageUrl){
+	getSocialCount(provider, pageUrl){
 		let result = {};
 		let deferred = this.$q.defer();
 		if (provider.type === "facebook"){
 			//find likes of pageURL from FB API
-			/*this.$http.get(this.facebookAPI).then(function(response) {
+			this.$http.get(this.facebookAPI).then(function(response) {
         		deferred.resolve(respone.data);
-    		});*/
+    		});
+			//deferred.resolve({"likes" : "1000"});
+		}
+		else if(provider.type === "pinterest"){
+			deferred.resolve({"pins" : "50"});	
 		}
 		//add pintertest
 		 return deferred.promise;
-	}
-
-	get getFacebookData(){
-		const provider = new socialProvider("facebook");
-		this.socialCountObj = this._getSocialCount(provider, "http://www.ynet.co.il");
-		return this.socialCountObj;
 	}
 
 }
