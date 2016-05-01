a80f652461d4d226362532d9c11f506e6c8fe3f3
adica <carmeladi@gmail.com>
2016-05-01 16:34:09 +0300

    move social count to component

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,30 +1,8 @@
-import socialProvider from './../../shared/socialProvider';
 
 class HomeController {
-    constructor(socialCountService) {
-        'ngInject';
-        this.name = 'home';
-        this.url = "http://www.google.com";
-        this.socialNetworksList = require('./../../shared/networks.json').networks;
-        this.selectedNetwork = "facebook"; //defualt value
-        this.socialCountService = socialCountService;
-        this.socialCountResult = {};
-    }
-
-    onGetData() {
-        const provider = new socialProvider(this.selectedNetwork);
-        this.socialCountService.getSocialCount(provider, this.url).then((data) => {
-            this.socialCountResult = data;
-        });
-    }
-
-    get socialCount() {
-        return this.socialCountResult;
-    }
-
-    set socialCount(val) {
-        this.socialCountResult = val;
-    }
+    constructor() {        
+        this.name = 'home';        
+    }    
 }
 
 export default HomeController;
--- client/app/components/socialcount/socialcount.controller.js
+++ client/app/components/socialcount/socialcount.controller.js
@@ -1,7 +1,30 @@
+import socialProvider from './../../shared/socialProvider';
+
 class SocialcountController {
-  constructor() {
-    this.name = 'socialcount';
-  }
+    constructor(socialCountService) {
+    	'ngInject';
+        this.name = 'socialcount';
+        this.url = "http://www.google.com"; //defualt value
+        this.selectedNetwork = "facebook"; //defualt value
+        this.socialNetworksList = require('./../../shared/networks.json').networks;        
+        this.socialCountService = socialCountService;
+        this.socialCountResult = {};
+    }
+
+    onGetData() {
+        const provider = new socialProvider(this.selectedNetwork);
+        this.socialCountService.getSocialCount(provider, this.url).then((data) => {
+            this.socialCountResult = data;
+        });
+    }
+
+    get socialCount() {
+        return this.socialCountResult;
+    }
+
+    set socialCount(val) {
+        this.socialCountResult = val;
+    }
 }
 
 export default SocialcountController;
